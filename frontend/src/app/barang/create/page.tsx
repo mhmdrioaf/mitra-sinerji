e659eda220b77a5d01505f8ba7a294bb40bd0e92
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-08-01 05:48:30 +0700

    feat(frontend): initialize the create barang page

---
+++ frontend/src/app/barang/create/page.tsx
@@ -0,0 +1,9 @@
+import FormBarang from "@/components/ui/forms/FormBarang";
+
+export default function CreateBarangPage() {
+  return (
+    <div className="w-full px-4 py-2 md:px-8 flex items-center min-h-[calc(100svh-6rem)] justify-center">
+      <FormBarang />
+    </div>
+  );
+}
