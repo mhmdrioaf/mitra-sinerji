b36853a8f6c6cdfd64396b04a999f91db9dde303
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-08-02 06:05:47 +0700

    feat(frontend): initialze the sales input components and handlers

---
+++ frontend/src/lib/api/sales/fetcher.ts
@@ -0,0 +1,16 @@
+import { ApiEndpoint } from "@/lib/constants";
+import { TGetSalesApiResponse, TListSalesApiResponse } from "./definitions";
+
+export async function listSales(): Promise<TListSalesApiResponse> {
+  const res = await fetch(ApiEndpoint.Sales.List);
+  const response: TListSalesApiResponse = await res.json();
+
+  return response;
+}
+
+export async function getSalesById(id: number): Promise<TGetSalesApiResponse> {
+  const res = await fetch(ApiEndpoint.Sales.Detail(id));
+  const response: TGetSalesApiResponse = await res.json();
+
+  return response;
+}
