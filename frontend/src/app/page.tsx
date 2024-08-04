0527394583e13d39b5a5da1e2b6f115a05979856
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-08-05 02:52:37 +0700

    perf(frontend): slightly improve `LCP` peformance by fetching dashboard data on the server

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,33 +1,122 @@
-import { QueryProvider } from "@/components/ui/QueryProvider";
+import {
+  CardContainer,
+  CardContent,
+  CardTitle,
+} from "@/components/ui/cards/CardContainer";
 import SmallLoader from "@/components/ui/SmallLoader";
-import React, { Suspense } from "react";
+import { listBarang } from "@/lib/api/barang/fetcher";
+import { listCustomer } from "@/lib/api/customer/fetcher";
+import { listSales } from "@/lib/api/sales/fetcher";
+import dynamic from "next/dynamic";
+import Link from "next/link";
+import { Suspense } from "react";
 
-const SalesChart = React.lazy(() => import("@/components/ui/SalesChart"));
-const DashboardBarangCard = React.lazy(
-  () => import("@/components/ui/cards/DashboardBarangCard")
+export const revalidate = 3600;
+
+const SalesChart = dynamic(() => import("@/components/ui/SalesChart"), {
+  loading: () => (
+    <div className="w-full md:max-w-4xl mx-auto h-[40svh] grid place-items-center">
+      <SmallLoader />
+    </div>
+  ),
+  ssr: false,
+});
+const DashboardBarangCard = dynamic(
+  () => import("@/components/ui/cards/DashboardBarangCard"),
+  {
+    loading: () => (
+      <div className="col-span-2 grid place-items-center">
+        <SmallLoader />
+      </div>
+    ),
+    ssr: false,
+  }
 );
-const DashboardCustomerCard = React.lazy(
-  () => import("@/components/ui/cards/DashboardCustomerCard")
+const DashboardCustomerCard = dynamic(
+  () => import("@/components/ui/cards/DashboardCustomerCard"),
+  {
+    loading: () => (
+      <div className="col-span-2 grid place-items-center">
+        <SmallLoader />
+      </div>
+    ),
+    ssr: false,
+  }
 );
 
 export default async function Home() {
+  const salesData = listSales();
+  const barangData = listBarang();
+  const customerData = listCustomer();
+
+  const [sales, barang, customer] = await Promise.all([
+    salesData,
+    barangData,
+    customerData,
+  ]);
+
   return (
     <div className="w-full px-4 md:px-8 py-2 md:py-6 min-h-[calc(100svh-4rem)] gap-8 flex flex-col text-primary">
-      <Suspense fallback={<SmallLoader />}>
-        <QueryProvider>
-          <SalesChart />
-        </QueryProvider>
-      </Suspense>
+      <div className="w-full md:max-w-4xl mx-auto min-h-[40svh] border rounded-md p-2">
+        <div className="flex flex-col gap-2 items-center">
+          <div className="flex flex-col gap-1 items-center">
+            <Link
+              href="/sales"
+              className="font-bold text-lg hover:underline hover:underline-offset-4"
+            >
+              Statistik Transaksi
+            </Link>
+            <p className="text-sm text-primary/75">
+              Berikut merupakan statistik data pendapatan dari seluruh transaksi
+            </p>
+          </div>
+
+          <Suspense fallback={<SmallLoader />}>
+            <SalesChart sales={sales.data} />
+          </Suspense>
+        </div>
+      </div>
 
       <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-8">
-        <Suspense fallback={<SmallLoader />}>
-          <QueryProvider>
-            <>
-              <DashboardBarangCard />
-              <DashboardCustomerCard />
-            </>
-          </QueryProvider>
-        </Suspense>
+        <CardContainer>
+          <CardTitle>
+            <Link
+              href="/barang"
+              className="hover:underline text-lg hover:underline-offset-4 font-bold"
+            >
+              Data Barang
+            </Link>
+            <p className="text-sm text-primary/75">
+              Berikut merupakan data barang yang tersedia
+            </p>
+          </CardTitle>
+
+          <CardContent>
+            <Suspense>
+              <DashboardBarangCard barang={barang.data} />
+            </Suspense>
+          </CardContent>
+        </CardContainer>
+
+        <CardContainer>
+          <CardTitle>
+            <Link
+              href="/customer"
+              className="hover:underline text-lg hover:underline-offset-4 font-bold"
+            >
+              Data Customer
+            </Link>
+            <p className="text-sm text-primary/75">
+              Berikut merupakan data customer yang tersedia
+            </p>
+          </CardTitle>
+
+          <CardContent>
+            <Suspense>
+              <DashboardCustomerCard customers={customer.data} />
+            </Suspense>
+          </CardContent>
+        </CardContainer>
       </div>
     </div>
   );
--- frontend/src/components/ui/cards/DashboardCustomerCard.tsx
+++ frontend/src/components/ui/cards/DashboardCustomerCard.tsx
@@ -1,67 +1,43 @@
 "use client";
 
-import { listCustomer } from "@/lib/api/customer/fetcher";
-import { useQuery } from "@tanstack/react-query";
+import { TCustomer } from "@/lib/api/customer/definitions";
 import Link from "next/link";
-import SmallLoader from "../SmallLoader";
 import { Button } from "../button";
-import { CardContainer, CardContent, CardTitle } from "./CardContainer";
 
-export default function DashboardCustomerCard() {
-  const query = useQuery({ queryKey: ["customer"], queryFn: listCustomer });
+interface IDashboardCustomerCardProps {
+  customers: TCustomer[];
+}
 
-  const data = query.data?.data || [];
-
+export default function DashboardCustomerCard({
+  customers,
+}: IDashboardCustomerCardProps) {
   return (
-    <CardContainer>
-      {data.length > 0 && (
-        <CardTitle>
-          <Link
-            href="/customer"
-            className="hover:underline text-lg hover:underline-offset-4 font-bold"
-          >
-            Data Customer
-          </Link>
-          <p className="text-sm text-primary/75">
-            Berikut merupakan data customer yang tersedia
+    <>
+      {customers.length < 1 ? (
+        <div className="col-span-2 flex flex-col gap-2 items-center justify-center">
+          <p className="text-center text-primary/65 text-xs">
+            Belum ada data customer yang ditambahkan
           </p>
-        </CardTitle>
-      )}
-      <CardContent>
-        {query.isLoading ? (
-          <div className="col-span-2 grid place-items-center">
-            <SmallLoader />
-          </div>
-        ) : query.error ? (
-          <p className="col-span-2 text-center text-primary/65 text-xs">
-            Terjadi kesalahan ketika mendapatkan data customer...
-          </p>
-        ) : data.length < 1 ? (
-          <div className="col-span-2 flex flex-col gap-2 items-center justify-center">
-            <p className="text-center text-primary/65 text-xs">
-              Belum ada data customer yang ditambahkan
-            </p>
-            <Button asChild variant="outline">
-              <Link href="/customer/create">Tambahkan Customer</Link>
-            </Button>
-          </div>
-        ) : (
-          data.slice(0, 4).map((item) => (
-            <Button asChild key={item.id} variant="outline">
-              <Link href={`/customer/${item.id}`}>
-                <p className="mr-2">{item.kode}</p>
-                <p>{item.name}</p>
-              </Link>
-            </Button>
-          ))
-        )}
-
-        {data.length > 0 && (
-          <Button asChild className="col-span-2 self-end">
-            <Link href="/customer">Lihat Semua Customer</Link>
+          <Button asChild variant="outline">
+            <Link href="/customer/create">Tambahkan Customer</Link>
           </Button>
-        )}
-      </CardContent>
-    </CardContainer>
+        </div>
+      ) : (
+        customers.slice(0, 4).map((item) => (
+          <Button asChild key={item.id} variant="outline">
+            <Link href={`/customer/${item.id}`}>
+              <p className="mr-2">{item.kode}</p>
+              <p>{item.name}</p>
+            </Link>
+          </Button>
+        ))
+      )}
+
+      {customers.length > 0 && (
+        <Button asChild className="col-span-2 self-end">
+          <Link href="/customer">Lihat Semua Customer</Link>
+        </Button>
+      )}
+    </>
   );
 }
