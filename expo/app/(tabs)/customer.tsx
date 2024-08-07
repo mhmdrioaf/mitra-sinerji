8bad3f84ba1d11ed12de41411ce20ab591b003ea
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-08-08 03:28:52 +0700

    init(mobile): mobile project initializations

---
+++ expo/app/(tabs)/customer.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function CustomerPage() {
+  return (
+    <SafeAreaView>
+      <Text>Customer</Text>
+    </SafeAreaView>
+  );
+}
---
+++ expo/app/(tabs)/transaksi.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function TransaksiPage() {
+  return (
+    <SafeAreaView>
+      <Text>Transaksi</Text>
+    </SafeAreaView>
+  );
+}
