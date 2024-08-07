25fd7fb057674bf7b5c0a3442b79ed34f2075dcf
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-08-08 06:06:25 +0700

    refactor(mobile): moving tabs layout to the tabs directory

---
+++ expo/app/(tabs)/_layout.tsx
@@ -0,0 +1,65 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useTheme } from "react-native-paper";
+import { createMaterialBottomTabNavigator } from "react-native-paper/react-navigation";
+import HomePage from ".";
+import BarangPage from "./barang";
+import CustomerPage from "./customer";
+import TransaksiPage from "./transaksi";
+
+const Tab = createMaterialBottomTabNavigator();
+
+export default function TabsLayout() {
+  const theme = useTheme();
+  return (
+    <Tab.Navigator initialRouteName="index" activeColor={theme.colors.primary}>
+      <Tab.Screen
+        name="index"
+        component={HomePage}
+        options={{
+          tabBarLabel: "Dashboard",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={22} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="barang"
+        component={BarangPage}
+        options={{
+          tabBarLabel: "Barang",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="package" color={color} size={22} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="customer"
+        component={CustomerPage}
+        options={{
+          tabBarLabel: "Customer",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="account-group"
+              color={color}
+              size={22}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="transaksi"
+        component={TransaksiPage}
+        options={{
+          tabBarLabel: "Transaksi",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="cash-register"
+              color={color}
+              size={22}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
